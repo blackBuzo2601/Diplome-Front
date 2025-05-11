70fe56ed3de22353fe9f92cb925bf3d391fd92c9
blackBuzo2601 <elianpro58@gmail.com>
2025-05-10 20:13:28 -0700

    Login listo con las proporciones lo más identicas posibles al canva

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,25 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.mainDiv}>
+    <form className={styles.mainDiv}>
       <div className={styles.form}>
-        <p className={styles.prueba}>Inicio sesión</p>
+        <p className={styles.mainTitle}>Inicio Sesión</p>
+        <p className={styles.subtitle}>
+          Desbloquea tu progreso: Inicia sesión.
+        </p>
+        <input
+          type="text"
+          placeholder="Correo electrónico"
+          required
+          className={styles.formInput}
+        />
+        <input
+          type="text"
+          placeholder="Contraseña"
+          required
+          className={styles.formInput}
+        />
       </div>
-    </div>
+    </form>
   );
 }
