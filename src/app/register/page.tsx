5c0645e3a8a3e18d1bf4e469dce2dab69161a1ff
blackBuzo2601 <elianpro58@gmail.com>
2025-05-10 23:24:54 -0700

    Pantalla registro casi lista

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,9 +3,57 @@ import Link from "next/link";
 
 export default function Register() {
   return (
-    <div>
-      <h1>Registrarse</h1>
-      <Link href="/">regresaar</Link>
+    <div className={styles.mainDiv}>
+      <form className={styles.form}>
+        <p className={styles.mainTitle}>Regístrate</p>
+        <p className={styles.subtitle}>¡Empieza a aprender gratis!</p>
+        <div className={styles.nameAndLastNameDiv}>
+          <input
+            type="text"
+            placeholder="Nombre"
+            required
+            className={styles.formInput}
+          />
+          <input
+            type="text"
+            placeholder="Apellido"
+            required
+            className={styles.formInput}
+          />
+        </div>
+        <div className={styles.ageDiv}>
+          <p className={styles.ageLabel}>Selecciona tu edad</p>
+          <select className={styles.selectAge} name="agenumber" required>
+            {Array.from({ length: 100 }, (_, i) => (
+              <option key={i + 1} value={i + 1}>
+                {i + 1}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={styles.restInformationContainer}>
+          <div className={styles.credentialsDiv}>
+            <input
+              type="email"
+              placeholder="Correo electrónico"
+              required
+              className={styles.credentialsInput}
+            />
+            <input
+              type="password"
+              placeholder="Contraseña"
+              required
+              className={styles.credentialsInput}
+            />
+            <input
+              type="password"
+              placeholder="Repetir contraseña"
+              required
+              className={styles.credentialsInput}
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
