030ca0ad7bb2c44bdd7fb1a1ad4ff08ce9b26bc1
blackBuzo2601 <elianpro58@gmail.com>
2025-05-10 19:06:53 -0700

    Añadida fuente personalizada de canva

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,5 +2,11 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.divPrincipal}>hola</div>;
+  return (
+    <div className={styles.mainDiv}>
+      <div className={styles.form}>
+        <p className={styles.prueba}>Inicio sesión</p>
+      </div>
+    </div>
+  );
 }
