f38303660ef43f3fc1b90ec0c53ac1ec019cfcd3
Coolhatena <ingsc.geffroy@gmail.com>
2025-05-11 22:32:02 -0700

    Connect login to backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,45 +2,61 @@
 import styles from "./page.module.css";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useAuth } from '../hooks/useAuth.js'
+import { AuthProvider } from "@/context/AuthContext";
+import { useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const router = useRouter();
+  const { login } = useAuth();
 
   //igual, por mientras pongo any para que no chille
-  const handleSubmit = (e: any) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
-
-    router.push("/dashboard");
+	try {
+		await login(email, password);
+		router.push("/dashboard");
+	} catch {
+		router.push("/");
+	}
   };
 
+
+
   return (
-    <form className={styles.mainDiv} onSubmit={handleSubmit}>
-      <div className={styles.form}>
-        <p className={styles.mainTitle}>Inicio Sesión</p>
-        <p className={styles.subtitle}>
-          Desbloquea tu progreso: Inicia sesión.
-        </p>
-        <input
-          type="email"
-          placeholder="Correo electrónico"
-          required
-          className={styles.formInput}
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          required
-          className={styles.formInput}
-        />
-        <button type="submit" className={styles.formButton}>
-          Entrar
-        </button>
-        <div className={styles.labelDiv}>
-          <Link className={styles.registerLabel} href="/register">
-            ¿No tienes cuenta? Regístrate aquí
-          </Link>
-        </div>
-      </div>
-    </form>
+		<form className={styles.mainDiv} onSubmit={handleSubmit}>
+		<div className={styles.form}>
+			<p className={styles.mainTitle}>Inicio Sesión</p>
+			<p className={styles.subtitle}>
+			Desbloquea tu progreso: Inicia sesión.
+			</p>
+			<input
+			type="email"
+			placeholder="Correo electrónico"
+			required
+			value={email}
+            onChange={(e) => setEmail(e.target.value)}
+			className={styles.formInput}
+			/>
+			<input
+			type="password"
+			placeholder="Contraseña"
+			required
+			value={password}
+            onChange={(e) => setPassword(e.target.value)}
+			className={styles.formInput}
+			/>
+			<button type="submit" className={styles.formButton}>
+			Entrar
+			</button>
+			<div className={styles.labelDiv}>
+			<Link className={styles.registerLabel} href="/register">
+				¿No tienes cuenta? Regístrate aquí
+			</Link>
+			</div>
+		</div>
+		</form>
   );
 }
