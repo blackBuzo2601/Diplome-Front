791f4a1100299c25c7acd4ec82151a3dc2a6a7bd
Coolhatena <ingsc.geffroy@gmail.com>
2025-05-11 22:30:44 -0700

    Protect dashboard page with auth access

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,19 @@
+"use client";
+import { useAuth } from "../../hooks/useAuth.js";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+	const { logout, currentUser } = useAuth();
+	
+	if (!currentUser) {
+		return <div>
+			<h1>403 Forbidden</h1>
+  	</div>;
+	}
+		
+	return <div>
+		<p>Dashboard</p>
+		<p>{`Usuario: ${currentUser.first_name} ${currentUser.last_name}`}</p>
+		<button onClick={() => logout()}>log out</button>
+  </div>;
+
 }
